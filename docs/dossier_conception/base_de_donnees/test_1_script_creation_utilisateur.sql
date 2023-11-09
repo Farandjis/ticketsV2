082effa074d1f7fbec4133af7976a0d19b3fb960
Matthieu <106311363+Linkieu@users.noreply.github.com>
2023-11-09 22:37:43 +0100

    Add files via upload
    Début d'un script de créations d'utilisateurs pour les essais

---
+++ docs/dossier_conception/base_de_donnees/test_1_script_creation_utilisateur.sql
@@ -0,0 +1,6 @@
+// A terminer, ajout en cours sur le serveur de secours
+
+INSERT INTO Utilisateur(ID_USER, LOGIN_USER, PRENOM_USER, NOM_USER, ROLE_USER, EMAIL_USER) VALUES(9991, "alice", "Alice", "AVRIL", "utilisateur", "alice.avril@email.com");
+INSERT INTO Utilisateur(ID_USER, LOGIN_USER, PRENOM_USER, NOM_USER, ROLE_USER, EMAIL_USER) VALUES(9992, "bobmorris", "Bob", "MORRIS", "utilisateur", "bob.morris@email.com");
+INSERT INTO Utilisateur(ID_USER, LOGIN_USER, PRENOM_USER, NOM_USER, ROLE_USER, EMAIL_USER) VALUES(9993, "roberto", "Roberto", "HONGO", "utilisateur", "roberto.hongo@email.com");
+INSERT INTO Utilisateur(ID_USER, LOGIN_USER, PRENOM_USER, NOM_USER, ROLE_USER, EMAIL_USER) VALUES(9994, "gordon", "Gordon", "SHUMWAY", "utilisateur", "alf@melmac.cat");
