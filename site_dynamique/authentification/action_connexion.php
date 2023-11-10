9403adb37d889a0826b7efc5528b3df49c8f5bad
Matthieu Linkieu <106311363+Linkieu@users.noreply.github.com>
2023-11-10 22:31:34 +0100

    modif du fichier : profil MariaDB fictif_connexionDB exécute les commandes et plus root. Fonctionne sur le serveur de secours.

--- site_dynamique/authentification/action_connexion.php
+++ site_dynamique/authentification/action_connexion.php
@@ -1,9 +1,10 @@
 <?php
 
 $host = 'localhost';
-$database = 'db_tix';
+$database = 'DB_TIX';
 
-$connection = mysqli_connect($host, 'root', '', $database);
+// $connection = mysqli_connect($host, 'root', '', $database);
+$connection = mysqli_connect($host, 'fictif_connexionDB', 't!nt1n_connexionDB45987645', $database);
 
 try {
     if (isset($_POST['login'], $_POST['mdp'])) {
@@ -14,16 +15,21 @@ try {
             throw new Exception("champ_vide");
         }
 
-        $requete = "SELECT ID_USER FROM utilisateur WHERE login_user = '$login'";
+        // $requete = "SELECT ID_USER FROM utilisateur WHERE login_user = '$login'";
+        $requete = "SELECT ID_USER FROM vue_UserFictif_connexionDB1 WHERE login_user = '$login'";
         $reponse = mysqli_query($connection, $requete);
 
         if ($row = mysqli_fetch_row($reponse)) {
             $connexion = mysqli_connect($host, $row[0], $mdp, $database);
 
             if ($connexion) {
+                /*
+                // MANQUE IP DERNIERE CONNEXION ! (10/11/2023)
+                // ATTENTION : IMPOSSIBLE D'UTILISER CETTE PARTIE ACTUELLEMENT : IMPOSSIBLE DE MODIFIER LA BD (le 10/11/2023)
                 $dateConnexion = date('Y-m-d H:i:s');
                 $updateRequete = "UPDATE utilisateur SET HORODATAGE_DERNIERE_CONNECTION_USER = '$dateConnexion' WHERE ID_USER = {$row[0]}";
                 mysqli_query($connection, $updateRequete);
+                */
 
                 session_start();
                 $_SESSION['login'] = $row[0];
