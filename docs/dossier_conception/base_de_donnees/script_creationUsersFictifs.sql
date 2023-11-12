9d6dac0d0ed2fad56be2e41eeeedc68f562b7b66
Enzo GUIGNOLLE <126868757+Enzo-Guignolle@users.noreply.github.com>
2023-11-12 15:59:57 +0100

    Ajout des user fictif pour la selection des données et l'update de l'horodatage de dernière connexion et l'IP de dernière connexion

--- docs/dossier_conception/base_de_donnees/script_creationUsersFictifs.sql
+++ docs/dossier_conception/base_de_donnees/script_creationUsersFictifs.sql
@@ -1,5 +1,7 @@
 CREATE USER 'fictif_connexionDB'@'localhost' IDENTIFIED BY 't!nt1n_connexionDB45987645';
 CREATE USER 'fictif_inscriptionDB'@'localhost' IDENTIFIED BY 't!nt1n_inscriptionDB45987645';
+CREATE USER 'fictif_selectionDB'@'localhost' IDENTIFIED BY 't!nt1n_selectionDB45987645';
+CREATE USER 'fictif_updateDB'@'localhost' IDENTIFIED BY 't!nt1n_updateDB45987645';
 
 // Accès à tous les identifiants et les logins des utilisateurs
 CREATE VIEW vue_UserFictif_connexionDB1 AS
@@ -8,6 +10,14 @@ FROM Utilisateur;
 
 CREATE VIEW vue_UserFictif_inscriptionDB1 AS 
 SELECT * 
+FROM Utilisateur;
+
+CREATE VIEW vue_UserFictif_selectionDB1 AS 
+SELECT *
+FROM Utilisateur;
+
+CREATE VIEW vue_UserFictif_updateDB1 AS 
+SELECT ID_USER, HORODATAGE_DERNIERE_CONNECTION_USER, IP_DERNIERE_CONNECTION_USER 
 FROM Utilisateur;
 
 /*
@@ -24,4 +34,8 @@ GRANT SELECT ON vue_UserFictif_connexionDB1 TO 'fictif_connexionDB'@'localhost';
 GRANT INSERT ON vue_UserFictif_inscriptionDB1 TO 'fictif_inscriptionDB'@'localhost';
 GRANT CREATE USER ON *.* TO 'fictif_inscriptionDB'@'localhost';
 GRANT GRANT OPTION, SELECT ON DB_TIX.* TO 'fictif_inscriptionDB'@'localhost';
+
+GRANT SELECT ON vue_UserFictif_selectionDB1 TO 'fictif_selectionDB'@'localhost';
+
+GRANT INSERT ON vue_UserFictif_updateDB1 TO 'fictif_updateDB'@'localhost';
 
