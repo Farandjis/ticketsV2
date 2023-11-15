631924ad1f9c714bdd5b1823b2a0341411700cd4
Enzo GUIGNOLLE <126868757+Enzo-Guignolle@users.noreply.github.com>
2023-11-15 18:34:09 +0100

    Ajout des vues et des users fictifs avec leur droit en rapport avec les tickets

--- docs/dossier_conception/base_de_donnees/script_creationUsersFictifs.sql
+++ docs/dossier_conception/base_de_donnees/script_creationUsersFictifs.sql
@@ -1,7 +1,9 @@
 CREATE USER 'fictif_connexionDB'@'localhost' IDENTIFIED BY 't!nt1n_connexionDB45987645';
 CREATE USER 'fictif_inscriptionDB'@'localhost' IDENTIFIED BY 't!nt1n_inscriptionDB45987645';
 CREATE USER 'fictif_selectionDB'@'localhost' IDENTIFIED BY 't!nt1n_selectionDB45987645';
-CREATE USER 'fictif_updateDB'@'localhost' IDENTIFIED BY 't!nt1n_updateDB45987645';
+CREATE USER 'fictif_updateTicketDB'@'localhost' IDENTIFIED BY 't!nt1n_updateTicketDB45987645';
+CREATE USER 'fictif_selectionTicketDB'@'localhost' IDENTIFIED BY 't!nt1n_selectionTicketDB45987645';
+CREATE USER 'fictif_insertionTicketDB'@'localhost' IDENTIFIED BY 't!nt1n_insertionTicketDB45987645';
 
 // Accès à tous les identifiants et les logins des utilisateurs
 CREATE VIEW vue_UserFictif_connexionDB1 AS
@@ -21,6 +23,18 @@ CREATE VIEW vue_UserFictif_updateDB1 AS
 SELECT ID_USER, HORODATAGE_DERNIERE_CONNECTION_USER, IP_DERNIERE_CONNECTION_USER 
 FROM Utilisateur;
 
+CREATE VIEW vue_UserFictif_updateTicketDB1 AS 
+SELECT ID_TICKET, OBJET_TICKET, DESCRIPTION_TICKET, ID_TECHNICIEN, NIV_URGENCE_DEFINITIF_TICKET
+FROM Ticket;
+
+CREATE VIEW vue_userfictif_selectionTicketDB1 AS 
+SELECT *
+FROM Ticket;
+
+CREATE VIEW vue_userfictif_insertionTicketDB1 AS 
+SELECT *
+FROM Ticket;
+
 GRANT SELECT ON vue_UserFictif_connexionDB1 TO 'fictif_connexionDB'@'localhost';
 
 GRANT INSERT ON vue_UserFictif_inscriptionDB1 TO 'fictif_inscriptionDB'@'localhost';
@@ -32,3 +46,8 @@ GRANT SELECT ON vue_UserFictif_selectionDB1 TO 'fictif_selectionDB'@'localhost';
 /*GRANT UPDATE ON vue_UserFictif_updateDB1 TO 'fictif_updateDB'@'localhost';*/ // A suppr ?
 GRANT UPDATE (HORODATAGE_DERNIERE_CONNECTION_USER, IP_DERNIERE_CONNECTION_USER) ON vue_UserFictif_updateDB1 TO fictif_connexionDB@localhost;
 
+GRANT UPDATE (OBJET_TICKET, DESCRIPTION_TICKET, ID_TECHNICIEN, NIV_URGENCE_DEFINITIF_TICKET) ON vue_UserFictif_updateTicketDB1 TO fictif_updateTicketDB@localhost;
+
+GRANT SELECT ON vue_UserFictif_selectionTicketDB1 TO fictif_selectionTicketDB@localhost;
+
+GRANT INSERT ON vue_UserFictif_insertionTicketDB1 TO fictif_insetionTicketDB@localhost;
