4372c5e6489289552acb78bd98ff52056b225918
Matthieu <106311363+Linkieu@users.noreply.github.com>
2023-11-17 22:53:20 +0100

    Update test_1_script_creation_utilisateur.sql : changement des ID_USER des users pour les tests

--- docs/dossier_conception/base_de_donnees/test_1_script_creation_utilisateur.sql
+++ docs/dossier_conception/base_de_donnees/test_1_script_creation_utilisateur.sql
@@ -1,44 +1,44 @@
 
--- A terminer, ajout en cours sur le serveur de secours
+-- script ne correspondant pas au S. Secours
 
 
-DROP USER '9991'@'localhost';
-DROP USER '9992'@'localhost';
-DROP USER '9993'@'localhost';
-DROP USER '9994'@'localhost';
+DROP USER '1'@'localhost'; -- alice
+DROP USER '2'@'localhost'; -- boblebricoleur
+DROP USER '3'@'localhost'; -- roberto
+DROP USER '4'@'localhost'; -- gordon
 
-DELETE FROM Utilisateur WHERE ID_USER = '9991';
-DELETE FROM Utilisateur WHERE ID_USER = '9992';
-DELETE FROM Utilisateur WHERE ID_USER = '9993';
-DELETE FROM Utilisateur WHERE ID_USER = '9994';
+DELETE FROM Utilisateur WHERE ID_USER = '1';
+DELETE FROM Utilisateur WHERE ID_USER = '2';
+DELETE FROM Utilisateur WHERE ID_USER = '3';
+DELETE FROM Utilisateur WHERE ID_USER = '4';
 
 INSERT INTO Utilisateur(ID_USER, LOGIN_USER, PRENOM_USER, NOM_USER, ROLE_USER, EMAIL_USER) VALUES(9991, "alice", "Alice", "AVRIL", "utilisateur", "alice.avril@email.com");
 INSERT INTO Utilisateur(ID_USER, LOGIN_USER, PRENOM_USER, NOM_USER, ROLE_USER, EMAIL_USER) VALUES(9992, "boblebricoleur", "Bob", "LEBRICOLEUR", "utilisateur", "lebricoleur@castorama.com");
 INSERT INTO Utilisateur(ID_USER, LOGIN_USER, PRENOM_USER, NOM_USER, ROLE_USER, EMAIL_USER) VALUES(9993, "roberto", "Roberto", "HONGO", "utilisateur", "roberto.hongo@email.com");
 INSERT INTO Utilisateur(ID_USER, LOGIN_USER, PRENOM_USER, NOM_USER, ROLE_USER, EMAIL_USER) VALUES(9994, "gordon", "Gordon", "SHUMWAY", "utilisateur", "alf@melmac.cat");
 
-CREATE USER '9991'@'localhost' IDENTIFIED BY 'azerty!123';
-CREATE USER '9992'@'localhost' IDENTIFIED BY 'azerty!123';
-CREATE USER '9993'@'localhost' IDENTIFIED BY 'azerty!123';
-CREATE USER '9994'@'localhost' IDENTIFIED BY 'azerty!123';
+CREATE USER '1'@'localhost' IDENTIFIED BY 'azerty!123';
+CREATE USER '2'@'localhost' IDENTIFIED BY 'azerty!123';
+CREATE USER '3'@'localhost' IDENTIFIED BY 'azerty!123';
+CREATE USER '4'@'localhost' IDENTIFIED BY 'azerty!123';
 
 
-GRANT SELECT ON vue_Utilisateur_client TO '9991'@'localhost';
-GRANT SELECT ON vue_Ticket_client TO '9991'@'localhost';
-GRANT UPDATE ON vue_Utilisateur_insertion_client TO '9991'@'localhost';
-GRANT UPDATE ON vue_Ticket_insertion_client TO '9991'@'localhost';
+GRANT SELECT ON vue_Utilisateur_client TO '1'@'localhost';
+GRANT SELECT ON vue_Ticket_client TO '1'@'localhost';
+GRANT UPDATE ON vue_Utilisateur_insertion_client TO '1'@'localhost';
+GRANT UPDATE ON vue_Ticket_insertion_client TO '1'@'localhost';
 
-GRANT SELECT ON vue_Utilisateur_client TO '9992'@'localhost';
-GRANT SELECT ON vue_Ticket_client TO '9992'@'localhost';
-GRANT UPDATE ON vue_Utilisateur_insertion_client TO '9992'@'localhost';
-GRANT UPDATE ON vue_Ticket_insertion_client TO '9992'@'localhost';
+GRANT SELECT ON vue_Utilisateur_client TO '2'@'localhost';
+GRANT SELECT ON vue_Ticket_client TO '2'@'localhost';
+GRANT UPDATE ON vue_Utilisateur_insertion_client TO '2'@'localhost';
+GRANT UPDATE ON vue_Ticket_insertion_client TO '2'@'localhost';
 
-GRANT SELECT ON vue_Utilisateur_client TO '9993'@'localhost';
-GRANT SELECT ON vue_Ticket_client TO '9993'@'localhost';
-GRANT UPDATE ON vue_Utilisateur_insertion_client TO '9993'@'localhost';
-GRANT UPDATE ON vue_Ticket_insertion_client TO '9993'@'localhost';
+GRANT SELECT ON vue_Utilisateur_client TO '3'@'localhost';
+GRANT SELECT ON vue_Ticket_client TO '3'@'localhost';
+GRANT UPDATE ON vue_Utilisateur_insertion_client TO '3'@'localhost';
+GRANT UPDATE ON vue_Ticket_insertion_client TO '3'@'localhost';
 
-GRANT SELECT ON vue_Utilisateur_client TO '9994'@'localhost';
-GRANT SELECT ON vue_Ticket_client TO '9994'@'localhost';
-GRANT UPDATE ON vue_Utilisateur_insertion_client TO '9994'@'localhost';
-GRANT UPDATE ON vue_Ticket_insertion_client TO '9994'@'localhost';
+GRANT SELECT ON vue_Utilisateur_client TO '4'@'localhost';
+GRANT SELECT ON vue_Ticket_client TO '4'@'localhost';
+GRANT UPDATE ON vue_Utilisateur_insertion_client TO '4'@'localhost';
+GRANT UPDATE ON vue_Ticket_insertion_client TO '4'@'localhost';
