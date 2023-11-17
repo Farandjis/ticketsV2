3a87c5a61f693756edc13add68bc05cbaed4a357
Matthieu <106311363+Linkieu@users.noreply.github.com>
2023-11-17 21:40:21 +0100

    Update 231112_patch_BD.sql : correction des commentaires SQL

--- docs/dossier_conception/base_de_donnees/231112_patch_BD.sql
+++ docs/dossier_conception/base_de_donnees/231112_patch_BD.sql
@@ -1,7 +1,7 @@
-// Ce script corrige l'absence d'une table "niveau durgence" du ticket.
-// https://sql.sh/cours/alter-table#google_vignette
-// https://blog.developpez.com/elsuket/p7900/moteur-de-base-de-donnees-sql-server/indexation/ajouter_des_contraintes_a_des_tables_dej
-// https://www.w3schools.com/sql/sql_default.asp
+-- Ce script corrige l'absence d'une table "niveau durgence" du ticket.
+-- https://sql.sh/cours/alter-table#google_vignette
+-- https://blog.developpez.com/elsuket/p7900/moteur-de-base-de-donnees-sql-server/indexation/ajouter_des_contraintes_a_des_tables_dej
+-- https://www.w3schools.com/sql/sql_default.asp
 
 CREATE TABLE UrgenceTicket(
 	VALEUR_URGENCE_TICKET VARCHAR(15) PRIMARY KEY
@@ -14,11 +14,11 @@ INSERT INTO UrgenceTicket (VALEUR_URGENCE_TICKET) VALUES
 	    ('Important'),
 	    ('Urgent');
 
-// Modification de la taille des valeurs des deux attributs	   
+-- Modification de la taille des valeurs des deux attributs	   
 ALTER TABLE Ticket MODIFY (NIV_URGENCE_ESTIMER_TICKET, VARCHAR(15));
 ALTER TABLE Ticket MODIFY (NIV_URGENCE_DEFINITIF_TICKET, VARCHAR(15));
 
-// Création des deux clés étrangères
+-- Création des deux clés étrangères
 ALTER TABLE Ticket
 	ADD CONSTRAINT NIV_URGENCE_ESTIMER_TICKET
   	FOREIGN KEY(NIV_URGENCE_ESTIMER_TICKET)
@@ -29,7 +29,7 @@ ALTER TABLE Ticket
   	FOREIGN KEY(NIV_URGENCE_DEFINITIF_TICKET)
   	REFERENCES UrgenceTicket(VALEUR_URGENCE_TICKET);
   	
-// On indique une valeur par défaut aux deux attributs
+-- On indique une valeur par défaut aux deux attributs
 ALTER TABLE Ticket
 	ALTER NIV_URGENCE_ESTIMER_TICKET SET DEFAULT 'Non complété !';
 	
