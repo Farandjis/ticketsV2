4dbf73e489bb62f3b9f90a4327a60953d22d50f8
Matthieu <106311363+Linkieu@users.noreply.github.com>
2023-11-17 22:53:43 +0100

    Update test_1_script_creation_utilisateur.sql : oubli pour les insert into

--- docs/dossier_conception/base_de_donnees/test_1_script_creation_utilisateur.sql
+++ docs/dossier_conception/base_de_donnees/test_1_script_creation_utilisateur.sql
@@ -12,10 +12,10 @@ DELETE FROM Utilisateur WHERE ID_USER = '2';
 DELETE FROM Utilisateur WHERE ID_USER = '3';
 DELETE FROM Utilisateur WHERE ID_USER = '4';
 
-INSERT INTO Utilisateur(ID_USER, LOGIN_USER, PRENOM_USER, NOM_USER, ROLE_USER, EMAIL_USER) VALUES(9991, "alice", "Alice", "AVRIL", "utilisateur", "alice.avril@email.com");
-INSERT INTO Utilisateur(ID_USER, LOGIN_USER, PRENOM_USER, NOM_USER, ROLE_USER, EMAIL_USER) VALUES(9992, "boblebricoleur", "Bob", "LEBRICOLEUR", "utilisateur", "lebricoleur@castorama.com");
-INSERT INTO Utilisateur(ID_USER, LOGIN_USER, PRENOM_USER, NOM_USER, ROLE_USER, EMAIL_USER) VALUES(9993, "roberto", "Roberto", "HONGO", "utilisateur", "roberto.hongo@email.com");
-INSERT INTO Utilisateur(ID_USER, LOGIN_USER, PRENOM_USER, NOM_USER, ROLE_USER, EMAIL_USER) VALUES(9994, "gordon", "Gordon", "SHUMWAY", "utilisateur", "alf@melmac.cat");
+INSERT INTO Utilisateur(ID_USER, LOGIN_USER, PRENOM_USER, NOM_USER, ROLE_USER, EMAIL_USER) VALUES(1, "alice", "Alice", "AVRIL", "utilisateur", "alice.avril@email.com");
+INSERT INTO Utilisateur(ID_USER, LOGIN_USER, PRENOM_USER, NOM_USER, ROLE_USER, EMAIL_USER) VALUES(2, "boblebricoleur", "Bob", "LEBRICOLEUR", "utilisateur", "lebricoleur@castorama.com");
+INSERT INTO Utilisateur(ID_USER, LOGIN_USER, PRENOM_USER, NOM_USER, ROLE_USER, EMAIL_USER) VALUES(3, "roberto", "Roberto", "HONGO", "utilisateur", "roberto.hongo@email.com");
+INSERT INTO Utilisateur(ID_USER, LOGIN_USER, PRENOM_USER, NOM_USER, ROLE_USER, EMAIL_USER) VALUES(4, "gordon", "Gordon", "SHUMWAY", "utilisateur", "alf@melmac.cat");
 
 CREATE USER '1'@'localhost' IDENTIFIED BY 'azerty!123';
 CREATE USER '2'@'localhost' IDENTIFIED BY 'azerty!123';
