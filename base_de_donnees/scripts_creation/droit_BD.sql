917560866efbadc73275acc0159bd5664a02ddb1
Matthieu <106311363+Linkieu@users.noreply.github.com>
2023-12-05 12:41:20 +0100

    Update droit_BD.sql : déplacement du don des droits à en bas de la page + ajout de @localhost après chaque nom des utilisateurs MariaDB

--- base_de_donnees/scripts_creation/droit_BD.sql
+++ base_de_donnees/scripts_creation/droit_BD.sql
@@ -6,9 +6,6 @@ CREATE VIEW vue_Ticket_visiteur AS
 SELECT HORODATAGE_CREATION_TICKET, OBJET_TICKET, NIV_URGENCE_ESTIMER_TICKET, DESCRIPTION_TICKET
 FROM Ticket;
 
-GRANT SELECT ON vue_Utilisateur_visiteur TO 'visiteur';
-GRANT SELECT ON vue_Ticket_visiteur TO 'visiteur';
-
 -- Utilisateur
 CREATE VIEW vue_Utilisateur_client AS
 SELECT ID_USER, LOGIN_USER, PRENOM_USER, NOM_USER, ROLE_USER, EMAIL_USER -- A modifier : supprimer ID_USER
@@ -67,15 +64,21 @@ GRANT UPDATE ON RelationTicketsLibelles TO 'role_admin_web';
 GRANT 'role_technicien' TO 'role_admin_sys';
 
 -- Création des utilisateurs
-CREATE USER '5' IDENTIFIED BY 'Assuranc3t0ur!x';
-CREATE USER '6' IDENTIFIED BY 'P0rqu3p!x';
-CREATE USER 'visiteur' IDENTIFIED BY 't9t+<Q33Pe%o4woPNw\D;hNdhZ}B/z';
+CREATE USER '5'@'localhost'; IDENTIFIED BY 'Assuranc3t0ur!x';
+CREATE USER '6'@'localhost'; IDENTIFIED BY 'P0rqu3p!x';
+CREATE USER 'visiteur'@'localhost'; IDENTIFIED BY 't9t+<Q33Pe%o4woPNw\D;hNdhZ}B/z';
 
+-- Ajout dans la table Utilisateurs les admins pour qu'ils puissent se connecter via la page de connexion
 INSERT INTO Utilisateur(ID_USER, LOGIN_USER, PRENOM_USER, NOM_USER, ROLE_USER, EMAIL_USER) VALUES(5, 'admin', 'Administrateur', 'SYSTEME', 'admin_sys', 'missing.sys@email.com');
 INSERT INTO Utilisateur(ID_USER, LOGIN_USER, PRENOM_USER, NOM_USER, ROLE_USER, EMAIL_USER) VALUES(6, 'gestion', 'Administrateur', 'WEB', 'admin_web', 'missing.web@email.com');
+
+-- Distribution des droits
+GRANT SELECT ON vue_Utilisateur_visiteur TO 'visiteur'@'localhost';
+GRANT SELECT ON vue_Ticket_visiteur TO 'visiteur'@'localhost';
 
 GRANT 'role_admin_sys' TO '5'@'localhost';
 GRANT 'role_admin_web' TO '5'@'localhost';
 
+-- Active par défaut les rôles pour les admins
 SET DEFAULT ROLE 'role_admin_sys' FOR '5'@'localhost';
 SET DEFAULT ROLE 'role_admin_web' FOR '6'@'localhost';
