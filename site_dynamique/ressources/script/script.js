ba59efae010cec4933dd2ef74eea6d22c34b1817
Matthieu Linkieu <106311363+Linkieu@users.noreply.github.com>
2023-12-09 15:16:12 +0100

    modif du style.css, ajout des scripts, ajout de l'icone modif.svg et background hero.jpg

---
+++ site_dynamique/ressources/script/script.js
@@ -0,0 +1,26 @@
+function toggleDropdown() {
+    var dropdown = document.getElementById("menu_deroulant_libelle");
+    dropdown.classList.toggle("active");
+}
+
+/*
+function setupForm(buttonId, overlayId) {
+    var showFormBtn = document.getElementById(buttonId);
+    var overlay = document.getElementsByClassName(overlayClass);
+
+    showFormBtn.addEventListener('click', function() {
+        overlay.style.display = 'flex';
+    });
+
+    window.addEventListener('click', function(event) {
+        if (event.target === overlay) {
+            overlay.style.display = 'none';
+        }
+    });
+}
+
+// Initialisation des formulaires
+setupForm('show-form-btn-1', 'overlay');
+setupForm('show-form-btn-2', 'overlay');
+ */
+
