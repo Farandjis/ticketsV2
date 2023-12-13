21f269e8c5394b5e61251b14a20748d71df903ab
Enzo GUIGNOLLE <126868757+Enzo-Guignolle@users.noreply.github.com>
2023-12-13 18:25:55 +0100

    Problème dans les droits

--- base_de_donnees/script_patch/patch_UF.sql
+++ base_de_donnees/script_patch/patch_UF.sql
@@ -1,8 +1,8 @@
 CREATE USER 'fictif_suppressionDB'@'localhost' IDENTIFIED BY 't!nt1n_suppressionDB45987645';
 
-GRANT DROP USER
+GRANT DROP
 ON DB_TIX.*
 TO 'fictif_suppressionDB'@'localhost';
 GRANT UPDATE(LOGIN_USER, PRENOM_USER, NOM_USER, ROLE_USER, EMAIL_USER)
-ON UTILISATEUR
+ON Utilisateur
 TO 'fictif_suppressionDB'@'localhost';
