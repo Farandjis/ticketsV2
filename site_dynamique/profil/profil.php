4290e83771f5d9f0665233a4a38458db88bc71d9
Enzo GUIGNOLLE <enzoguignolle13@gmail.com>
2023-12-17 13:25:03 +0100

    Modification de la page avec les fonctions récemment créé

--- site_dynamique/profil/profil.php
+++ site_dynamique/profil/profil.php
@@ -1,3 +1,10 @@
+<?php
+require '../ressources/fonctions/PHPfunctions.php';
+global $host, $database, $USER_FICTIF_MDP;
+
+$connexionUtilisateur = pageAccess(array('Utilisateur', 'Technicien', 'Administrateur Site', 'Administrateur Système'));
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -8,43 +15,9 @@
     <link rel="shortcut icon" href="../ressources/images/logo_sans_texte.png" type="image/x-icon">
 </head>
 <body class="profile">
-  <header>
-      <nav>
-          <div class="logo">
-              <img src="../ressources/images/logo_blanc.png" alt="logo du site">
-          </div>
-          <div class="nav-conteneur">
-          <a href="../" aria-current="page">Accueil</a>
           <?php
-          require '../ressources/fonctions/PHPfunctions.php';
-          global $host, $database, $USER_FICTIF_MDP;
-          $connexion = mysqli_connect($host, 'fictif_connexionDB', $USER_FICTIF_MDP['fictif_connexionDB'], $database);
-
-          session_start();
-          if (isset($_SESSION['login'], $_SESSION['mdp'])){
-              $loginSite = $_SESSION['login'];
-              $resSQL = mysqli_fetch_row(executeSQL("SELECT ID_USER FROM UserFictif_connexionDB1 WHERE login_user = ?", array($loginSite), $connexion));
-              mysqli_close($connexion);
-              $loginMariaDB = $resSQL[0];
-              $mdp = $_SESSION['mdp'];
-              $connexion = mysqli_connect($host, $loginMariaDB, $mdp, $database);
-              $role = recupererRoleDe($connexion);
-              if ($role == 'Administrateur Système') {
-                  echo "<a href='../logs/journalActivite.html'>Journal d'activité</a>
-                        <a href='../logs/Historique.html'>Historique</a>";
-                  echo "<a href='../tableau_bord/tableauBord.php'>Tableau de bord</a>";
-              }
-              else
-                  echo "<a href='../tableau_bord/tableauBord.php'>Tableau de bord</a>";
-          }
+          affichageMenuDuHaut("profil", $connexionUtilisateur);
           ?>
-          </div>
-          <div class="deconnexion">
-              <a href="../authentification/action_deconnexion.php">Déconnexion</a>
-          </div>
-      </nav>
-  </header>
-
   <h1 class="titre_page">
       Mon compte
   </h1>
@@ -54,9 +27,10 @@
             <?php
             $entete_profile = array('Login', 'Nom Complet', 'Email', 'Role','Mot de passe');
             $requete = "SELECT login_user,  nom_user, email_user, role_user,  prenom_user FROM vue_Utilisateur_client;";
-            $reponse =  mysqli_query($connexion, $requete);
+            $reponse =  mysqli_query($connexionUtilisateur, $requete);
             $row = mysqli_fetch_row($reponse);
             $row[1] = $row[4] . " " . $row[1];
+            $row[3] = recupererRoleDe($connexionUtilisateur);
             $row[4] = "*******";
             echo "<table class='table-perso'>
                 <tbody>";
@@ -95,7 +69,7 @@
                 <?php
                     $reqSQL = "SELECT DATE_FORMAT(HORODATAGE_CREATION_TICKET, 'le %d/%m/%Y à %Hh%i'), OBJET_TICKET, NIV_URGENCE_ESTIMER_TICKET,NIV_URGENCE_DEFINITIF_TICKET, DESCRIPTION_TICKET, ETAT_TICKET FROM vue_Ticket_client ORDER BY HORODATAGE_CREATION_TICKET DESC";
 
-                $getResultSQL = mysqli_query($connexion, $reqSQL);
+                $getResultSQL = mysqli_query($connexionUtilisateur, $reqSQL);
 
                 tableGenerate($getResultSQL);
                 ?>
