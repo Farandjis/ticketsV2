9701650727ddfd4087b02f7793e881206bfb2437
Matthieu <106311363+Linkieu@users.noreply.github.com>
2023-12-17 16:33:49 +0100

    Update patch_fonctions.sql : ajout de 2 fonctions : recup_etat_ticket_tdb et verifier_id_ticket_dans_vue_tdb

--- base_de_donnees/script_patch/patch_fonctions.sql
+++ base_de_donnees/script_patch/patch_fonctions.sql
@@ -12,6 +12,51 @@ END //
 
 DELIMITER ;
 
+
+
+
+
+DROP FUNCTION IF EXISTS verifier_id_ticket_dans_vue_tdb;
+DELIMITER //
+
+-- Fonction qui vérifie qu'un ticket est présent dans le tableau de bord de l'utilisateur
+CREATE FUNCTION verifier_id_ticket_dans_vue_tdb(id_ticket_param INT) RETURNS INT
+BEGIN
+    DECLARE ticket_exists INT;
+
+    -- Vérifier si l'id_ticket se trouve dans la vue_tableau_bord
+    SELECT COUNT(*) INTO ticket_exists
+    FROM vue_tableau_bord
+    WHERE id_ticket = id_ticket_param;
+
+    -- Retourner 1 si l'id_ticket existe dans la vue, sinon retourner 0
+    RETURN IF(ticket_exists > 0, 1, 0);
+END //
+
+DELIMITER ;
+
+
+
+DROP FUNCTION IF EXISTS recup_etat_ticket_tdb;
+
+DELIMITER //
+
+-- Fonction qui récupère du tableau de bord l'état d'un ticket demandé par l'utilisateur
+CREATE FUNCTION recup_etat_ticket_tdb(id_ticket_param INT) RETURNS VARCHAR(30)
+BEGIN
+    DECLARE etat_ticket_recup VARCHAR(30);
+
+    -- Vérifier si l'id_ticket se trouve dans la vue_tableau_bord
+    SELECT etat_ticket INTO etat_ticket_recup
+    FROM vue_tableau_bord
+    WHERE id_ticket = id_ticket_param;
+
+    -- Retourner l'état du ticket s'il existe dans la vue
+    RETURN etat_ticket_recup;
+END //
+
+DELIMITER ;
+
 GRANT EXECUTE ON FUNCTION ObtenirRoleUtilisateur TO 'role_utilisateur';
 GRANT EXECUTE ON FUNCTION ObtenirRoleUtilisateur TO 'role_technicien';
 GRANT EXECUTE ON FUNCTION ObtenirRoleUtilisateur TO 'role_admin_sys';
