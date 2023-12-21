12a54a2c668674d462558f4230c21f760a2235d4
Enzo GUIGNOLLE <enzoguignolle13@gmail.com>
2023-12-21 12:42:12 +0100

    Correction de la page modifTicket à cause d'une ancienne version commit

--- site_dynamique/tableau_bord/modificationTicket.php
+++ site_dynamique/tableau_bord/modificationTicket.php
@@ -1,12 +1,12 @@
 <?php
-	require (dirname(__FILE__) . "/../ressources/fonctions/PHPfunctions.php");
-	
-	$connection = pageAccess(array('Utilisateur', 'Technicien', 'Administrateur Site', 'Administrateur Système'));
-	if (!isset($_POST["id_ticket"]) || empty($_POST["id_ticket"])){
-		header('Location: tableaudebord.php');
-	}
-	$id_ticket = $_POST["id_ticket"];
-	$info_ticket = mysqli_fetch_array(executeSQL("	SELECT id_crea,
+require (dirname(__FILE__) . "/../ressources/fonctions/PHPfunctions.php");
+
+$connection = pageAccess(array('Utilisateur', 'Technicien', 'Administrateur Site', 'Administrateur Système'));
+if (!isset($_POST["id_ticket"]) || empty($_POST["id_ticket"])){
+    header('Location: tableaudebord.php');
+}
+$id_ticket = $_POST["id_ticket"];
+$info_ticket = mysqli_fetch_array(executeSQL("	SELECT id_crea,
 													etat_ticket,
 													prenom_crea,
 													nom_crea,
@@ -20,21 +20,19 @@
 													prenom_tech,
 													nom_tech
 													FROM vue_tableau_bord WHERE id_ticket = ?;",array($id_ticket),$connection));
-	
-	// (TOUT LE MONDE) TRUE si ticket en attente de l'utilisateur
-        $modifPossible = (boolean)mysqli_fetch_row(executeSQL("SELECT COUNT(ID_TICKET) FROM vue_modif_creation_ticket_utilisateur WHERE ID_TICKET = ?;", array($id_ticket), $connection))[0];
 
-        if (in_array(recupererRoleDe($connection), array("Technicien", "Administrateur Site"))) {
-        	$modifPossibleAdmTech = (boolean)mysqli_fetch_row(executeSQL("SELECT COUNT(ID_TICKET) FROM vue_modif_ticket_adm_tech WHERE ID_TICKET = ?;", array($id_ticket), $connection))[0];
+// (TOUT LE MONDE) TRUE si ticket en attente de l'utilisateur
+$modifPossibleUti = (boolean)mysqli_fetch_row(executeSQL("SELECT COUNT(ID_TICKET) FROM vue_modif_creation_ticket_utilisateur WHERE ID_TICKET = ?;", array($id_ticket), $connection))[0];
 
-        	// (TECH / ADMIN SITE) : TRUE s'il est modifiable par le technicien ou l'administrateur faisant la demande
-        	$modifPossible = ($modifPossible or $modifPossibleAdmTech);
-	}
-	if(! $modifPossible){
-		pageAccess(array()); // A remplacer par un vrai truc au propre mais c'est juste pour que ça fonctionne
-	}
+if (in_array(recupererRoleDe($connection), array("Technicien", "Administrateur Site"))) {
+    $modifPossibleAdmTech = (boolean)mysqli_fetch_row(executeSQL("SELECT COUNT(ID_TICKET) FROM vue_modif_ticket_adm_tech WHERE ID_TICKET = ?;", array($id_ticket), $connection))[0];
 
-	
+}
+if(! ($modifPossibleUti or $modifPossibleAdmTech)){ // TRUE (avec ! -> False) s'il est modifiable par le technicien, l'utilisateur ou l'administrateur faisant la demande
+    pageAccess(array()); // A remplacer par un vrai truc au propre mais c'est juste pour que ça fonctionne
+}
+
+
 
 
 /*
@@ -65,30 +63,30 @@ $user_id = mysqli_fetch_array($connection->query("SELECT id_user, prenom_user, n
     <script src="../ressources/script/libelle.js"></script>
 </head>
 <body>
-    <header>
-        <div class="retour">
-            <a href="javascript:window.history.go(-1)"><img src="../ressources/images/fleche_retour.png" alt="bouton retour"> Retour</a>
-        </div>
-    </header>
-    <div class="page_cree-modif_ticket">
-        <h1 class="h1Creation">Modification de Ticket (le 19/12 à 09h53 : tout fonctionne)</h1>
-        <div role="form" class="formModifTicket formAuthentification formConnexion">
-<?php
-		echo "
+<header>
+    <div class="retour">
+        <a href="javascript:window.history.go(-1)"><img src="../ressources/images/fleche_retour.png" alt="bouton retour"> Retour</a>
+    </div>
+</header>
+<div class="page_cree-modif_ticket">
+    <h1 class="h1Creation">Modification de Ticket (le 19/12 à 09h53 : tout fonctionne)</h1>
+    <div role="form" class="formModifTicket formAuthentification formConnexion">
+        <?php
+        echo "
             <div class='informations_ticket'>
                 <div class='info-ticket-gauche'>
                 <p>ID TICKET : $id_ticket</p>";
-				if (recupererRoleDe($connection) == 'Utilisateur' || recupererRoleDe($connection) == 'Administrateur Système'){
-					echo "
+        if (recupererRoleDe($connection) == 'Utilisateur' || recupererRoleDe($connection) == 'Administrateur Système'){
+            echo "
 					<p>ID USER : $user_id[0]</p>
 					<p>Utilisateur : $user_id[1] $user_id[2]</p>";
-				}
-				else{
-					echo "
+        }
+        else{
+            echo "
 					<p>ID USER : $info_ticket[0]</p>
 					<p>Utilisateur : $info_ticket[2] $info_ticket[3]</p>";
-				}
-				echo "
+        }
+        echo "
                 </div>
 
                 <div class='info-ticket-droite'>
@@ -98,150 +96,152 @@ $user_id = mysqli_fetch_array($connection->query("SELECT id_user, prenom_user, n
                 </div>
             </div>
 			";
-?>
-            <form action='action_modificationTicket.php' method='post'>
-                <div class="form-modif-ticket">
-                    <div class="champs_ticket_gauche">
-                        <label for='login'>Nature du problème *</label><br>
-                        <input id='nature' type='text' name='nature' value="<?php echo htmlspecialchars(htmlspecialchars_decode($info_ticket[6]));?>">
+        ?>
+        <form action='action_modificationTicket.php' method='post'>
+            <div class="form-modif-ticket">
+                <div class="champs_ticket_gauche">
+                    <label for='login'>Nature du problème *</label><br>
+                    <input id='nature' type='text' name='nature' value="<?php echo htmlspecialchars(htmlspecialchars_decode($info_ticket[6]));?>">
 
-                        <br><br>
+                    <br><br>
 
-                        <label for='explication2'>Explication</label><br>
-                        <textarea id="explication2" name="explication2" ><?php 
-						echo "$info_ticket[7]";
-						?></textarea><br>
-                    </div>
+                    <label for='explication2'>Explication</label><br>
+                    <textarea id="explication2" name="explication2" ><?php
+                        echo "$info_ticket[7]";
+                        ?></textarea><br>
+                </div>
 
-                    <div class="champs_ticket_droit">
-                        <p>Niveau d'urgence estimé</p>
-						<?php
-						// recupererRoleDe($connection) == 'Utilisateur'
-						if ($user_id[0] == $info_ticket[0] && $info_ticket[1] == "En attente"){
-							echo "
+                <div class="champs_ticket_droit">
+                    <p>Niveau d'urgence estimé</p>
+                    <?php
+                    // recupererRoleDe($connection) == 'Utilisateur'
+                    // $user_id[0] == $info_ticket[0] && $info_ticket[1] == "En attente"
+
+                    if ($modifPossibleUti){
+                        echo "
 							<div class='custom-select'>
 							<select name='nivUrg' id='nivUrg' required>
 								<option value=''>--Choisir une option--</option>
 								<option value='Faible'";
-								echo isSelected('Faible',$info_ticket[8]);
-								echo " 
+                        echo isSelected('Faible',$info_ticket[8]);
+                        echo " 
 								>Faible</option>
 								<option value='Moyen'
 								";
-								echo isSelected('Moyen',$info_ticket[8]);
-								echo " 
+                        echo isSelected('Moyen',$info_ticket[8]);
+                        echo " 
 								>Moyen</option>
 								<option value='Important'
 								";
-								echo isSelected('Important',$info_ticket[8]);
-								echo "
+                        echo isSelected('Important',$info_ticket[8]);
+                        echo "
 								>Important</option>
 								<option value='Urgent'
 								";
-								echo isSelected('Urgent',$info_ticket[8]);
-								echo " 
+                        echo isSelected('Urgent',$info_ticket[8]);
+                        echo " 
 								>Urgent</option>
 							</select>
 							</div>
 							";
-						}
-						else{
-							echo '<input type="hidden" name="nivUrg" value="<?php echo $info_ticket[8]; ?>">';
-							echo "<p id='champLocked'>$info_ticket[8]</p>";
-						}
-						?>
-						<br>
-                        <label for='nivUrg2'>Niveau d'urgence</label><br>
-						<?php
-						if (recupererRoleDe($connection) == 'Administrateur Site'){
-							echo "
+                    }
+                    else{
+                        echo '<input type="hidden" name="nivUrg" value="<?php echo $info_ticket[8]; ?>">';
+                        echo "<p id='champLocked'>$info_ticket[8]</p>";
+                    }
+                    ?>
+                    <br>
+                    <label for='nivUrg2'>Niveau d'urgence</label><br>
+                    <?php
+                    if (recupererRoleDe($connection) == 'Administrateur Site'){
+                        echo "
 							<div class='custom-select'>
 							<select name='nivUrg2' id='nivUrg2' required>
 								<option value=''>--Choisir une option--</option>
 								<option value='Faible'";
-								echo isSelected('Faible',$info_ticket[9]);
-								echo " 
+                        echo isSelected('Faible',$info_ticket[9]);
+                        echo " 
 								>Faible</option>
 								<option value='Moyen'
 								";
-								echo isSelected('Moyen',$info_ticket[9]);
-								echo " 
+                        echo isSelected('Moyen',$info_ticket[9]);
+                        echo " 
 								>Moyen</option>
 								<option value='Important'
 								";
-								echo isSelected('Important',$info_ticket[9]);
-								echo "
+                        echo isSelected('Important',$info_ticket[9]);
+                        echo "
 								>Important</option>
 								<option value='Urgent'
 								";
-								echo isSelected('Urgent',$info_ticket[9]);
-								echo " 
+                        echo isSelected('Urgent',$info_ticket[9]);
+                        echo " 
 								>Urgent</option>
 							</select>
 							</div>
 							";
-						}
-						else{
-							echo '<input type="hidden" name="nivUrg2" value="<?php echo $info_ticket[9]; ?>">';
-							echo "<p id='champLocked'>$info_ticket[9]</p>";
-						}
-						?>
+                    }
+                    else{
+                        echo '<input type="hidden" name="nivUrg2" value="<?php echo $info_ticket[9]; ?>">';
+                        echo "<p id='champLocked'>$info_ticket[9]</p>";
+                    }
+                    ?>
 
-                        <br>
-                        <label for='ch_technicien'>Technicien affecté</label><br>
-						<?php
-						if (recupererRoleDe($connection) == 'Administrateur Site'){
-							$liste_technicien = $connection->query('SELECT id_user,prenom_user,nom_user FROM vue_technicien;');
-							echo "
+                    <br>
+                    <label for='ch_technicien'>Technicien affecté</label><br>
+                    <?php
+                    if (recupererRoleDe($connection) == 'Administrateur Site'){
+                        $liste_technicien = $connection->query('SELECT id_user,prenom_user,nom_user FROM vue_technicien;');
+                        echo "
 							<div class='custom-select'>
 								<select name='ch_technicien' id='ch_technicien'>
 									<option value=''>--Choisir une option--</option>
 									";
-									while ($row = mysqli_fetch_row($liste_technicien)){
-										echo "<option value='$row[0]'";
-										echo isSelected($row[0],$info_ticket[10]);
-										echo "
+                        while ($row = mysqli_fetch_row($liste_technicien)){
+                            echo "<option value='$row[0]'";
+                            echo isSelected($row[0],$info_ticket[10]);
+                            echo "
 										>$row[1] $row[2]</option>";
-									}
-									echo "
+                        }
+                        echo "
 								</select>
 							</div>
 							";
-						}
-						else {
-							if (!empty($info_ticket[10])){
-								echo '<input type="hidden" name="ch_technicien" value="<?php echo $info_ticket[10]; ?>">';
-								echo "<p id='champLocked'>$info_ticket[11] $info_ticket[12]</p>";
-							}
-							else{
-								echo "<p id='champLocked'>Aucun</p>";
-							}
-						}
-						?>
-						<br>
+                    }
+                    else {
+                        if ((!empty($info_ticket[11])) and (!empty($info_ticket[12]))){
+                            echo '<input type="hidden" name="ch_technicien">'; // value="< ?php echo $info_ticket[10]; ? >"
+                            echo "<p id='champLocked'>$info_ticket[11] $info_ticket[12]</p>";
+                        }
+                        else{
+                            echo "<p id='champLocked'>Aucun</p>";
+                        }
+                    }
+                    ?>
+                    <br>
 
-                        <span>Libellé</span><br>
-                        <div class="menu_libelle" id="menu_deroulant_libelle">
-                            <span class="entete_libelle modif_entete_libelle" onclick="toggleDropdown()">--Liste des libellés--</span>
-                            <div class="option_libelle">
-								<?php
-								$listeLibelle = array();
-								$result = executeSQL("SELECT nom_libelle FROM vue_tdb_relation_ticket_libelle WHERE id_ticket = ?",array($id_ticket),$connection);
-								while($row = mysqli_fetch_array($result)){
-									array_push($listeLibelle,$row[0]);
-								}
-								menuDeroulantTousLesLibelles($connection,$listeLibelle);
-								?>
+                    <span>Libellé</span><br>
+                    <div class="menu_libelle" id="menu_deroulant_libelle">
+                        <span class="entete_libelle modif_entete_libelle" onclick="toggleDropdown()">--Liste des libellés--</span>
+                        <div class="option_libelle">
+                            <?php
+                            $listeLibelle = array();
+                            $result = executeSQL("SELECT nom_libelle FROM vue_tdb_relation_ticket_libelle WHERE id_ticket = ?",array($id_ticket),$connection);
+                            while($row = mysqli_fetch_array($result)){
+                                array_push($listeLibelle,$row[0]);
+                            }
+                            menuDeroulantTousLesLibelles($connection,$listeLibelle);
+                            ?>
 
 
-                            </div>
                         </div>
                     </div>
                 </div>
-				<input type="hidden" name="id_ticket" value='<?php echo $id_ticket; ?>'>
-                <input type='submit' name='modif' value='Modifier le ticket'><br>
-            </form>
-        </div>
+            </div>
+            <input type="hidden" name="id_ticket" value='<?php echo $id_ticket; ?>'>
+            <input type='submit' name='modif' value='Modifier le ticket'><br>
+        </form>
     </div>
+</div>
 </body>
 </html>
