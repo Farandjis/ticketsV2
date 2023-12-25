2301e11b3171b79ce103114ee383641b8e6b4e8c
Matthieu <106311363+Linkieu@users.noreply.github.com>
2023-12-25 23:10:00 +0100

    accueil : oubli d'une correction suite au changement de fichier (TITRE_TICKET et plus OBJET_TICKET)

--- site_dynamique/index.php
+++ site_dynamique/index.php
@@ -84,7 +84,7 @@ if (isset($_SESSION['login'], $_SESSION['mdp'])) {
 
     $connexion = mysqli_connect($host, 'visiteur', $USER_FICTIF_MDP['visiteur'], $database);
 
-    $reqSQL = "SELECT ID_Ticket, DATE_FORMAT(HORODATAGE_CREATION_TICKET, 'le %d/%m/%Y à %Hh%i'), OBJET_TICKET, NIV_URGENCE_DEFINITIF_TICKET, DESCRIPTION_TICKET, ETAT_TICKET FROM vue_Ticket_visiteur";
+    $reqSQL = "SELECT ID_Ticket, DATE_FORMAT(HORODATAGE_CREATION_TICKET, 'le %d/%m/%Y à %Hh%i'), TITRE_TICKET, NIV_URGENCE_DEFINITIF_TICKET, DESCRIPTION_TICKET, ETAT_TICKET FROM vue_Ticket_visiteur";
 
     $getResultSQL = mysqli_query($connexion, $reqSQL);
     if (! (mysqli_num_rows($getResultSQL) == 0)){ // S'il y a au moins 1 ticket à affiché, on affiche la section
