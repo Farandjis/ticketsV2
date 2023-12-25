f664b72eede009d0bdcce3eedb366eaa4a7a8364
Matthieu <106311363+Linkieu@users.noreply.github.com>
2023-12-25 20:02:38 +0100

    remise au propre de la branche site_dynamique

---
+++ site_dynamique/administration/action_telechargement.php
@@ -0,0 +1,35 @@
+<?php
+
+require (dirname(__FILE__) . "/../ressources/fonctions/PHPfunctions.php");
+$connection = pageAccess(array('Administrateur Site', 'Administrateur Système'));
+
+if (isset($_POST["journal"]) && !empty($_POST["journal"])){
+	if (recupererRoleDe($connection) == 'Administrateur Système'){
+		if ($_POST["journal"] == "connexion_infructueuse"){
+			header('Location: journauxActvCreTck.csv');
+		}
+		if ($_POST["journal"] == "ouverture_ticket"){
+			header('Location: journauxActvCoInf.csv');
+		}
+		if ($_POST["journal"] == "historique"){
+			$result = $connection->query("SELECT HORODATAGE_CREATION_TICKET,HORODATAGE_DERNIERE_MODIF_TICKET,OBJET_TICKET,NIV_URGENCE_DEFINITIF_TICKET,DESCRIPTION_TICKET,ID_USER,ID_TECHNICIEN FROM vue_historique");
+
+			$csvFileName = "logs/temp_historique.csv";
+			$csvFile = fopen($csvFileName, 'w');
+
+			$fields = $result->fetch_fields();
+			$header = [];
+			foreach ($fields as $field) {
+				$header[] = $field->name;
+			}
+			fputcsv($csvFile, $header);
+			while ($row = $result->fetch_assoc()) {
+				fputcsv($csvFile, $row);
+			}
+			fclose($csvFile);
+			$connection->close();
+			header('Location: logs/temp_historique.csv');
+		}
+	}
+}
+?>
--- site_dynamique/ressources/info_db/user_ficif.php
+++
@@ -1,3 +0,0 @@
-<?php
-$USER_FICTIF_MDP= array("fictif_connexionDB" => 't!nt1n_connexionDB45987645');
-?>
