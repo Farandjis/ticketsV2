79d0ebb621501b2d191f46d93c7b722f0c41a688
Matthieu <106311363+Linkieu@users.noreply.github.com>
2023-12-28 20:04:27 +0100

    Create test_2_script_creation_libelles.sql : ajout de quelques cas

---
+++ base_de_donnees/scripts_test/test_2_script_creation_libelles.sql
@@ -0,0 +1,11 @@
+-- Souris manquante G24
+INSERT INTO RelationTicketsMotscles VALUES('4', "Salle INFO : G24");
+INSERT INTO RelationTicketsMotscles VALUES('4', "Matériel : Souris");
+
+-- Robotnik a piraté ma session
+INSERT INTO RelationTicketsMotscles VALUES('2', "Système : Windows");
+
+-- Il n'y a plus de chat dans le frigo
+INSERT INTO RelationTicketsMotscles VALUES('6', "Matériel : Autre");
+INSERT INTO RelationTicketsMotscles VALUES('6', "Matériel : Prise Électrique");
+INSERT INTO RelationTicketsMotscles VALUES('6', "Salle INFO : Autre");
