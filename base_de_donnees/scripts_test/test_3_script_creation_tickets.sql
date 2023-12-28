f493d5bd114b6defe9868ffb130a831f270c4dab
Matthieu <106311363+Linkieu@users.noreply.github.com>
2023-12-28 10:30:07 +0100

    Create test_3_script_creation_tickets.sql : quelques tests pour moi pour la BD V2

---
+++ base_de_donnees/scripts_test/test_3_script_creation_tickets.sql
@@ -0,0 +1,8 @@
+-- A exécuter en tant que n'importe quel utilisateur MariaDB
+-- Incomplet
+
+INSERT INTO `vue_modif_creation_ticket_utilisateur`(`TITRE_TICKET`, `DESCRIPTION_TICKET`, `NIV_URGENCE_ESTIMER_TICKET`) VALUES ('[AUTRE] Piratage','Robotnik s\'est connecté à ma session !','Urgent');
+INSERT INTO `vue_modif_creation_ticket_utilisateur`(`TITRE_TICKET`, `DESCRIPTION_TICKET`, `NIV_URGENCE_ESTIMER_TICKET`) VALUES ('[AUTRE] Piratage','Je crois qu\'on m\'a piraté, mes fichiers ont disparus','Urgent');
+INSERT INTO `vue_modif_creation_ticket_utilisateur`(`TITRE_TICKET`, `DESCRIPTION_TICKET`, `NIV_URGENCE_ESTIMER_TICKET`) VALUES ('[MATERIEL] Matériel manquant','Il manque une souris en G24','Faible');
+INSERT INTO `vue_modif_creation_ticket_utilisateur`(`TITRE_TICKET`, `DESCRIPTION_TICKET`, `NIV_URGENCE_ESTIMER_TICKET`) VALUES ('[MATERIEL] Matériel en panne','L\'ordinateur ne s\'allume plus','Moyen');
+INSERT INTO `vue_modif_creation_ticket_utilisateur`(`TITRE_TICKET`, `DESCRIPTION_TICKET`, `NIV_URGENCE_ESTIMER_TICKET`) VALUES ('[!] Autre problème','Il n\'y a plus de chat dans le frigo','Urgent');
