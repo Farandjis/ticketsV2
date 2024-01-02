f201ccce0c6363b0f4d1a4406e616da75f2b0392
Florent <128831864+FlorentVASSEURBERLIOUX@users.noreply.github.com>
2024-01-02 18:52:14 +0100

    Mise à jour de la page action_ajouttechnicien

--- site_dynamique/administration/action_ajouttechnicien.php
+++ site_dynamique/administration/action_ajouttechnicien.php
@@ -3,9 +3,21 @@
 require (dirname(__FILE__) . "/../ressources/fonctions/PHPfunctions.php");
 $connection = pageAccess(array('Administrateur Site'));
 
-
-if (isset($_POST["ajout_motcle"]) && !empty($_POST["ajout_motcle"])){
-	executeSQL("INSERT INTO `MotcleTicket` (`NOM_MOTCLE`) VALUES (?);",array($_POST["ajout_motcle"]),$connection);
+if (isset($_POST["tech_option"]) && !empty($_POST["tech_option"])){
+	foreach ($_POST["tech_option"] as $userID) {
+		echo $userID;
+		$userID = htmlspecialchars($userID);
+		$grantPrivilege = $connection->query("GRANT 'role_technicien' TO '$userID'@localhost;");
+		$roleUpdate = $connection->query("CALL activerUnRoleTechOuUtiParAdminWeb($userID, 'role_technicien');");
+	}
+	
+	$totTechnicien = $connection->query("SELECT ID_USER FROM affiche_utilisateurs_pour_adm_web;");
+	while ($row = mysqli_fetch_row($totTechnicien)) {
+	$userID = $row[0];
+		if (!(in_array($userID,$_POST["tech_option"]))){
+			$roleUpdate = $connection->query("CALL activerUnRoleTechOuUtiParAdminWeb($userID, 'role_utilisaeur');");
+		}
+	}
 }
 
 header('Location: administration.php');
