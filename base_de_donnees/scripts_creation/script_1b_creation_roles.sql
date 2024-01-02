c7c2563257169e521736782e62688865a5439221
Matthieu <106311363+Linkieu@users.noreply.github.com>
2024-01-02 11:04:08 +0100

    Update script_1b_creation_roles.sql : ajout drop role if exists

--- base_de_donnees/scripts_creation/script_1b_creation_roles.sql
+++ base_de_donnees/scripts_creation/script_1b_creation_roles.sql
@@ -1,3 +1,8 @@
+DROP ROLE IF EXISTS 'role_technicien';
+DROP ROLE IF EXISTS 'role_utilisateur';
+DROP ROLE IF EXISTS 'role_admin_web';
+DROP ROLE IF EXISTS 'role_admin_sys';
+
 CREATE ROLE 'role_technicien';
 CREATE ROLE 'role_utilisateur';
 CREATE ROLE 'role_admin_web';
