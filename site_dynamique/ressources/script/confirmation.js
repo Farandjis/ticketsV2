22322e19b524863f97ba7c5b053eb18df9c708c8
Florent <128831864+FlorentVASSEURBERLIOUX@users.noreply.github.com>
2024-01-30 13:31:46 +0100

    Mise à jour des pop-ups pour la séparation de la gestion des techniciens

--- site_dynamique/ressources/script/confirmation.js
+++ site_dynamique/ressources/script/confirmation.js
@@ -1,23 +1,25 @@
 function confirmerAvantEnvoi(elName) {
     // Ajoutez ici la logique pour retourner le bon texte selon le nom du formulaire submit
+	
     switch(elName) {
         case 'Modifier le ticket':
             return window.confirm("Êtes-vous sûr de vouloir modifier ce ticket ?");
         case 'Finir le ticket':
             return window.confirm("Êtes-vous sûr de vouloir finir ce ticket ?");
         case 'Ajout Technicien':
-            return window.confirm("Êtes-vous sûr de vouloir modifier ce ou ces technicien(s) ?")
-        case 'Ajout Libelle':
-            return window.confirm("Êtes-vous sûr de vouloir ajouter ce mot-clé ?")
+            return window.confirm("Êtes-vous sûr de vouloir ajouter ce technicien ?");
+		case 'Suppression Technicien':
+			return window.confirm("Êtes-vous sûr de vouloir supprimer ce technicien ?");
+		case 'Ajout Libelle':
+            return window.confirm("Êtes-vous sûr de vouloir ajouter ce mot-clé ?");
         case 'Suppression Motcle':
-            return window.confirm("Êtes-vous sûr de vouloir supprimer ce ou ces mot(s) clé(s) ?")
+            return window.confirm("Êtes-vous sûr de vouloir supprimer ce ou ces mot(s) clé(s) ?");
         case 'Ajout Titre':
-            return window.confirm("Êtes-vous sûr de vouloir ajouter ce titre ?")
+            return window.confirm("Êtes-vous sûr de vouloir ajouter ce titre ?");
         case 'Suppression Titre':
-            return window.confirm("Êtes-vous sûr de vouloir supprimer ce ou ces titre(s) ?")
-
+            return window.confirm("Êtes-vous sûr de vouloir supprimer ce ou ces titre(s) ?");
 
         default:
-            return false
+            return alert("Mauvais nom de formulaire !");
     }
 }
