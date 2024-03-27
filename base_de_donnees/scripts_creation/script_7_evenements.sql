f93fdf8236b884f425baba18a84296918329b415
Matthieu Linkieu <106311363+Linkieu@users.noreply.github.com>
2024-03-27 21:15:41 +0100

    script_7_evenements.sql : Ajout évènement manquant : DEBAN_AUTO_COMPTES_ECHEANCE_PASSER

--- base_de_donnees/scripts_creation/script_7_evenements.sql
+++ base_de_donnees/scripts_creation/script_7_evenements.sql
@@ -17,3 +17,18 @@ CREATE EVENT SUPPRESSION_AUTO_COMPTES_INACTIFS
     	CALL ATTENTION_SupprimerTousLesComptesInutilises();	-- On supprime tous les comptes inactifs depuis au moins 36 mois.
 END //
 DELIMITER ;
+
+
+-- ===================================================== DEBANISSEMENT AUTOMATIQUE DES COMPTES DONT L'ECHEANCE DU BAN EST PASSÉ TOUS LES JOURS À 1H DU MATIN
+
+DROP EVENT IF EXISTS DEBAN_AUTO_COMPTES_ECHEANCE_PASSER;
+
+DELIMITER //
+CREATE EVENT DEBAN_AUTO_COMPTES_ECHEANCE_PASSER
+    ON SCHEDULE EVERY 1 DAY 					-- Tous les jours
+    STARTS CURRENT_DATE + INTERVAL 1 DAY + INTERVAL 1 HOUR	-- A partir de demain 1h
+    DO
+    BEGIN
+    	CALL debanComptesEcheancePasser();	-- On débanni tous les comptes dont l'échéance est passé.
+END //
+DELIMITER ;
